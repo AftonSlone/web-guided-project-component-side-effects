e1e7d3d1ebd2e915471f8fab22499aa39618399d
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-09 12:02:58 -0600

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ export default function App() {
   // The effect should consist of a call to the API using axios.
   // On success, set the array of friend objects from the API into state.
   useEffect(() => {
-    axios.get().then().catch();
+    axios.get(`${BASE_URL}/friends?api_key=${API_KEY}`).then().catch();
   }, []);
 
   const Friend = (props) => (
