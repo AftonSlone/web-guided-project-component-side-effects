4a5db6f9905e557bb8f22666fbc2ded26996aa68
Afton Slone <afton.slone@gmail.com>
2020-12-20 02:00:30 -0500

    Finished Guided Project

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,39 +1,78 @@
-import React, { useState, useEffect } from 'react'
-import { BASE_URL, API_KEY } from '../constants'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import { BASE_URL, API_KEY } from "../constants";
+import axios from "axios";
 
 export default function Details(props) {
-  const { friendId, close } = props
-  const [details, setDetails] = useState(null)
+  const { friendId, close } = props;
+  const [details, setDetails] = useState(null);
 
   // 👉 TASK 4 - Create a side effect 🥇 that runs only after first render.
+  useEffect(() => {
+    console.log(`EFFECT ONLY AFTER FIRST RENDER`);
+    return () => {
+      console.log(`CLEANIP RIGHT BEFORE COMPONENT IS REMOVED`);
+    };
+  }, []);
 
   // 👉 TASK 5 - Create a side effect 👻 that runs only after first render
+  // useEffect(() => {
+  //   const listener = (evt) => {
+  //     console.log(`Here is a random number ${Math.random()}`);
+  //   };
+  //   document.addEventListener("click", listener);
+  //   return () => {
+  //     document.removeEventListener("click", listener);
+  //   };
+  // }, []);
+
   // and puts a 'click' event handler on document.
   // See what happens if we don't clean up.
 
   // 👉 TASK 6 - Create a side effect 🥵 that runs after every render.
 
+  useEffect(() => {
+    console.log(`Runs after first rendar + all others too`);
+    return () => {
+      console.log(
+        `cleanup after the effect of the Previous rendor of the component`
+      );
+    };
+  }, []);
+
   // 👉 TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/friends/${friendId}?api_key=${API_KEY}`)
+      .then((res) => {
+        setDetails(res.data);
+      })
+      .catch((err) => {
+        debugger;
+      });
+  }, [friendId]);
   // Whenever props.friendId updates we should trigger a fetch for details of the friend.
   // The URL should end up looking like `http://localhost:4000/friends/1?api_key=xyz`
   // On success, shove the details of the friend in `details` slice of state
 
   return (
-    <div className='container'>
+    <div className="container">
       <h2>Details (of friend with id {friendId}):</h2>
-      {
-        details &&
+      {details && (
         <>
-          <p>{details.name} is {details.age}</p>
+          <p>
+            {details.name} is {details.age}
+          </p>
           <p>email is {details.email}</p>
           {name} likes:
           <ul>
-            {details.hobbies.map((hobby) => <li key={hobby}>{hobby}</li>)}
+            {details.hobbies.map((hobby) => (
+              <li key={hobby}>{hobby}</li>
+            ))}
           </ul>
         </>
-      }
+      )}
       <button onClick={close}>Close</button>
     </div>
-  )
+  );
 }
